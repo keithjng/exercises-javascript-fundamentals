4b866760ca8b61aae369238145b67671af0f1693
Keith Ng <keith.jk.ng@gmail.com>
2020-01-16 10:38:00 -0800

    Add files via upload

--- exercises/numbers/isPositive/isPositive.js
+++ exercises/numbers/isPositive/isPositive.js
@@ -1,20 +1,24 @@
-/**
- * Returns true if the given number is positive (greater than 0) and false otherwise.
- *
- * @param {number} num The input number
- * @returns {boolean} True if num is positive and false otherwise
- */
+//trying to display truth for positive and false for non-positives
+
+//let num = process.argv[2]
+
+//if (num > 0) {
+//    console.log ('True');
+//} else {
+//    console.log ('False')
+//}
+
 function isPositive(num) {
-  return _____;
-}
-
-if (require.main === module) {
-  console.log('Running sanity checks for isPositive:');
-
-  console.log(isPositive(0) === false);
-  console.log(isPositive(1) === true);
-  console.log(isPositive(-1) === false);
-  console.log(isPositive(-100.20) === false);
-}
-
-module.exports = isPositive;
+    return (num > 0);
+  }
+  
+  if (require.main === module) {
+    console.log('Running sanity checks for isPositive:');
+  
+    console.log(isPositive(0) === false);
+    console.log(isPositive(1) === true);
+    console.log(isPositive(-1) === false);
+    console.log(isPositive(-100.20) === false);
+  }
+  
+  module.exports = isPositive;
