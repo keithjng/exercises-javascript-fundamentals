eb3a7be23b43937759bab73ceac7a00a9c85ba28
Keith Ng <keith.jk.ng@gmail.com>
2020-01-18 16:41:48 -0800

    Add files via upload

--- exercises/strings/longestWord/longestWord.js
+++ exercises/strings/longestWord/longestWord.js
@@ -9,10 +9,24 @@
 
 function longestWord(string) {
   // This is your job. :)
+  var split = string.split(' '); //breaks by spaces 
+  var num = 0;
+    for (var i = 0; i < split.length; i++){ 
+      if (num < split[i].length){
+        num = split[i].length;
+        var longest = split[i];
+      }
+    } 
+    console.log(longest)
+    return longest
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for longestWord:');
+  
+  console.log(longestWord('wither creeper chicken enderdragon') === 'enderdragon');
+  console.log(longestWord('lost frequencies is nice to listen to') === 'frequencies');
+  console.log(longestWord('I ordered ramen today') === 'ordered');
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
