81440525b2887d2d3484b1caad677a31fa9fa68c
Keith Ng <keith.jk.ng@gmail.com>
2020-01-18 16:51:29 -0800

    Add files via upload

--- exercises/strings/shortestWord/shortestWord.js
+++ exercises/strings/shortestWord/shortestWord.js
@@ -9,11 +9,24 @@
 
 function shortestWord(string) {
   // This is your job. :)
+  var split = string.split(' '); 
+  var num = string.length; 
+    for (var i = 0; i < split.length; i++){ 
+      if (num > split[i].length){
+        num = split[i].length;
+        var shortest = split[i];
+      }
+    } 
+    return shortest
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for shortestWord:');
 
+  console.log(shortestWord('wither creeper chicken enderdragon') === 'wither');
+  console.log(shortestWord('lost frequencies within worldviews') === 'lost');
+  console.log(shortestWord('I ordered ramen today') === 'I'); 
+  console.log(shortestWord('Tom Cruise in Mission Impossible') === 'in')
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
