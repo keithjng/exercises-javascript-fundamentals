1836b504de2ab3cd1de9f9063a1a74ec4561dc5c
Keith Ng <keith.jk.ng@gmail.com>
2020-01-18 22:10:46 -0800

    Add files via upload

--- exercises/strings/countWords/countWords.js
+++ exercises/strings/countWords/countWords.js
@@ -9,10 +9,17 @@
 
 function countWords(string) {
   // This is your job. :)
+  split = string.split(' ').length; 
+  return split
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for countWords:');
+
+  console.log(countWords('mine yours ours theirs') === 4) 
+  console.log(countWords('how are you today my friend') === 6)
+  console.log(countWords('who what where when why how') === 6)
+  console.log(countWords('I do not know how to do any of this') === 10)
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
