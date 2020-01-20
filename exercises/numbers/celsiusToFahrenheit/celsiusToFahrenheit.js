d82c707020acdbc7b834f3ac19d301b1d9720f57
Keith Ng <keith.jk.ng@gmail.com>
2020-01-19 21:27:58 -0800

    Add files via upload

--- exercises/numbers/celsiusToFahrenheit/celsiusToFahrenheit.js
+++ exercises/numbers/celsiusToFahrenheit/celsiusToFahrenheit.js
@@ -6,10 +6,19 @@
  */
 function celsiusToFahrenheit(temp) {
   // This is your job. :).
+  var NF = 9 / 5
+  let fahrenheit = temp * NF + 32
+  return fahrenheit
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for celsiusToFahrenheit:');
+
+  console.log(celsiusToFahrenheit(0) === 32)
+  console.log(celsiusToFahrenheit(10) === 50)
+  console.log(celsiusToFahrenheit(32) === 89.6)
+  console.log(celsiusToFahrenheit(-32) === -25.6)
+  console.log(celsiusToFahrenheit(40) === 104)
 
   // Your sanity checks go here.
 }
