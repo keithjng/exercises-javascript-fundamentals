a3772c109836eb2a6274aed61fc11aeb1b1734b8
Keith Ng <keith.jk.ng@gmail.com>
2020-01-20 11:15:56 -0800

    Add files via upload

--- exercises/numbers/isPrime/isPrime.js
+++ exercises/numbers/isPrime/isPrime.js
@@ -20,6 +20,15 @@ function isPrime(num) {
     and use pen/paper, index cards, etc. — anything that helps you think
     about it without getting stuck in JavaScript syntax.
   */
+ if (num === 1) {
+  return false
+}
+  for (let i = 2; i < num; i++){
+    if (num % i === 0){
+      return false
+    }  
+  }
+  return true
 }
 
 if (require.main === module) {
@@ -28,6 +37,12 @@ if (require.main === module) {
   console.log(isPrime(1) === false);
   console.log(isPrime(2) === true);
   console.log(isPrime(4) === false);
+  console.log(isPrime(5) === true);
+  console.log(isPrime(6) === false);
+  console.log(isPrime(13) === true);
+  console.log(isPrime(17) === true);
+  console.log(isPrime(21) === false);
+  console.log(isPrime(251) === true);
 
   // Your own sanity checks go here
 }
